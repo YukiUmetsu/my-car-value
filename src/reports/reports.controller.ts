dd9d9602beb27e21cd7c85f504b3f178b2ba9527
YukiUmetsu <yuuki.umetsu@gmail.com>
2022-06-27 11:25:35 -0500

    create a report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Report } from './report.entity';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+
+    constructor(private reportsService: ReportsService) {}
+
+    @Post()
+    @UseGuards(AuthGuard)
+    createReport(@Body() body: CreateReportDto): Promise<Report> {
+        return this.reportsService.create(body);
+    }
+}
