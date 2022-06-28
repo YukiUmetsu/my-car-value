cc21d3bf537342ed2d2c6dc51008c8815ef28f96
YukiUmetsu <yuuki.umetsu@gmail.com>
2022-06-28 10:13:40 -0500

    add admin guard to report approve endpoint, add get estimate endpoint
    add admin guard to report approve endpoint, add get estimate endpoint

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
+import { AdminGuard } from 'src/guards/admin.guard';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CurrentUser } from 'src/users/decorators/current-user.decorators';
 import { User } from 'src/users/user.entity';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { ReportDto } from './dtos/report.dto';
 import { Report } from './report.entity';
 import { ReportsService } from './reports.service';
@@ -22,7 +24,13 @@ export class ReportsController {
     }
 
     @Patch('/:id')
+    @UseGuards(AdminGuard)
     approveReport(@Param('id') id: string, @Body() body: ApproveReportDto): Promise<Report> {
         return this.reportsService.changeApproval(id, body.approved);
+    }
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto) {
+        return this.reportsService.createEstimate(query);
     }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptors';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 
 @Module({
   imports: [
@@ -15,11 +14,10 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
   providers: [
     UsersService, 
     AuthService,
-    {
-      // global interceptor
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
-    }
   ]
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*')
+  }
+}
